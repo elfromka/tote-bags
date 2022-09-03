2205c984bf74f73c068e7661fab1994aebb4f354
Sebestyen Orsolya <elfromka@gmail.com>
2022-09-03 14:12:00 +0300

    Installed sass and normalize.css, set up basic scss style files, added favicon and some meta data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
+import "./assets/scss/styles.scss";
+
 const App = () => {
-    return <div className="App"></div>;
+    return <div className="App">Tote Bags store</div>;
 };
 
 export default App;
